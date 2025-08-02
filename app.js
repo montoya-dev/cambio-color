bf25ff82d421b688e3aa947265fda5f6503bfe26
montoya-dev <montoya-dev@users.noreply.github.com>
2025-08-01 22:47:42 -0600

    Se deja la app funcionando

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const boton = document.querySelector('button')
-const color = document.getElementById('color')
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
 
-function generadorcolor () {
-    
+function generadorcolor() {
+  let digitos = '0123456789ABCDEF';
+  let colorHex = '#';
+
+  for (let i = 0; i < 6; i++) {
+    let indiceAleatorio = Math.floor(Math.random() * 16);
+    colorHex += digitos[indiceAleatorio];
+  }
+
+  return colorHex;
 }
+
+boton.addEventListener('click', function () {
+  let colorAleatorio = generadorcolor();
+  color.textContent = colorAleatorio;
+  document.body.style.backgroundColor = colorAleatorio
+
+})
+
